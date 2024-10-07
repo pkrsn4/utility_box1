98af20adb399a9122379cfdd50fd2f68c3e098bf
W Saahil <saahilgazi@gmail.com>
2024-10-07 13:53:53 +0530

    Update access_code_repo.py

--- access_code_repo.py
+++ access_code_repo.py
@@ -1,30 +1,42 @@
+computer_vision=False
+com_path=False
+pytorch=False
+tensorflow=False
+
 import os
 import sys
 import time
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
-import cv2
-from tifffile import imread, imsave
 from pathlib import Path
-from pma_python import core
 
-code_repo_path = f'{prefix}/UtilityBox'
-sys.path.append(code_repo_path)
+sys.path.append(f'UtilityBox')
+
+if tensorflow:
+    import utils.tf_gpu_utils as tfgu
+
+if pytorch:
+    import utils.torch_gpu_utils as tgu
+
+if computer_vision:
+    import utils.image_utils as iu
+    import ocv
+    import cv2
+    import Shapely
+    
+if com_path:
+    from tifffile import imread, imsave
+    import pma as pu
+    import compath.tissue as tu
+    from pma_python import core
 
 import utils.utils as u
-import utils.torch_gpu_utils as tgu
-import utils.tf_gpu_utils as tfgu
-import utils.image_utils as iu
-import load 
-import pma as pu
-import Shapely
-import ocv
-import compath.tissue as tu
-
+import load
 import s3
+
+ 
 '''
 contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 hierarchy = [[[ 1, -1, -1, -1],  # Contour 0: [Next, Prev, First_Child, Parent]
